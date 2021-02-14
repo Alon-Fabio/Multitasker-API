930db9237c02fc310f5bd53aaf2e9cb9336908b0
Alon-Fabio <alon.the.fabio@gmail.com>
2021-02-14 18:48:12 +0200

    Redis update

--- controllers/signin.js
+++ controllers/signin.js
@@ -2,9 +2,28 @@ const jwt = require("jsonwebtoken");
 const { REDIS_URI, JWT_SECRET } = require("../secret");
 
 const redis = require("redis");
-const redisClient = redis.createClient(
-  process.env.REDIS_URI ? process.env.REDIS_URI : REDIS_URI
-);
+const redisProsURI = process.env.REDIS_URI || REDIS_URI;
+const redisClient = redis.createClient({
+  redisProsURI,
+  retry_strategy: function (options) {
+    if (options.error && options.error.code === "ECONNREFUSED") {
+      // End reconnecting on a specific error and flush all commands with
+      // a individual error
+      return new Error("The server refused the connection");
+    }
+    if (options.total_retry_time > 1000 * 60 * 60) {
+      // End reconnecting after a specific timeout and flush all commands
+      // with a individual error
+      return new Error("Retry time exhausted");
+    }
+    if (options.attempt > 10) {
+      // End reconnecting with built in error
+      return undefined;
+    }
+    // reconnect after
+    return Math.min(options.attempt * 100, 3000);
+  },
+});
 
 const dataFetch = (db, bcrypt, req) => {
   const { email, password } = req.body;
