ee89a99329e28442ec523d717b4bc8aa86e1b69e
Alon-Fabio <alon.the.fabio@gmail.com>
2021-02-19 14:07:20 +0200

    Docker redis update

--- controllers/signin.js
+++ controllers/signin.js
@@ -3,27 +3,7 @@ const { REDIS_URI, JWT_SECRET } = require("../secret");
 
 const redis = require("redis");
 const redisProsURI = process.env.REDIS_URI || REDIS_URI;
-const redisClient = redis.createClient({
-  redisProsURI,
-  retry_strategy: function (options) {
-    if (options.error && options.error.code === "ECONNREFUSED") {
-      // End reconnecting on a specific error and flush all commands with
-      // a individual error
-      return new Error("The server refused the connection");
-    }
-    if (options.total_retry_time > 1000 * 60 * 60) {
-      // End reconnecting after a specific timeout and flush all commands
-      // with a individual error
-      return new Error("Retry time exhausted");
-    }
-    if (options.attempt > 10) {
-      // End reconnecting with built in error
-      return undefined;
-    }
-    // reconnect after
-    return Math.min(options.attempt * 100, 3000);
-  },
-});
+const redisClient = redis.createClient(redisProsURI);
 
 const dataFetch = (db, bcrypt, req) => {
   const { email, password } = req.body;
--- dockerfile
+++ dockerfile
@@ -1,11 +1,10 @@
 FROM node:15.8.0
+# FROM node:12.13.0
 
 WORKDIR /usr/src/Multitasker-api
 
 COPY ./ ./
 
 RUN npm install
-
-EXPOSE 3000 6379
 
 CMD ["sh"]
