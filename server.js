a027bb4ce89e00dcba8664f181d93388ebbfe519
Alon-Fabio <alon.the.fabio@gmail.com>
2021-02-20 18:43:09 +0200

    Docker update

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cors = require("cors");
 const knex = require("knex");
 const morgan = require("morgan");
 
-// const register = require("./controllers/register");
-// const signin = require("./controllers/signin");
-// const profile = require("./controllers/profile");
-// const image = require("./controllers/image");
-// const auth = require("./controllers/auth");
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+const auth = require("./controllers/auth");
 
 const { POSTGRES_URI } = require("./secret");
 
@@ -36,25 +36,25 @@ app.use(morgan("combined"));
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/", (res, req) => {
+app.get("/", (req, res) => {
   res.send("It's working and updating !");
 });
-// app.post("/signin", signin.signinAuthentication(db, bcrypt));
-// app.post("/register", (req, res) => {
-//   register.handleRegister(req, res, db, bcrypt);
-// });
-// app.get("/profile/:id", auth.getAuthentication, (req, res) => {
-//   profile.handleProfileGet(req, res, db);
-// });
-// app.post("/profile/:id", auth.getAuthentication, (req, res) => {
-//   profile.handleProfilePost(req, res, db);
-// });
-// app.put("/image", auth.getAuthentication, (req, res) => {
-//   image.handleImage(req, res, db);
-// });
-// app.post("/imageurl", auth.getAuthentication, (req, res) => {
-//   image.handleApiCall(req, res);
-// });
+app.post("/signin", signin.signinAuthentication(db, bcrypt));
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get("/profile/:id", auth.getAuthentication, (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.post("/profile/:id", auth.getAuthentication, (req, res) => {
+  profile.handleProfilePost(req, res, db);
+});
+app.put("/image", auth.getAuthentication, (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post("/imageurl", auth.getAuthentication, (req, res) => {
+  image.handleApiCall(req, res);
+});
 
 app.listen(8080, () => {
   console.log("app is running on port 8080");
