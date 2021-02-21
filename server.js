326e5e664fff96ce364c2c88b6f25f518acbf016
Alon-Fabio <alon.the.fabio@gmail.com>
2021-02-21 21:39:26 +0200

    Dockerfile update

--- server.js
+++ server.js
@@ -37,6 +37,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
+  const handleProfileGet = () => {
+    db.select("*")
+      .from("users")
+      .then(console.log)
+      .catch((err) => res.status(400).json("error getting user"));
+  };
+  handleProfileGet();
   res.send("It's working and updating !");
 });
 app.post("/signin", signin.signinAuthentication(db, bcrypt));
