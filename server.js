a80df73f6a347a95fe34316124218aa9cdd22ed1
Alon-Fabio <alon.the.fabio@gmail.com>
2021-03-01 16:55:10 +0200

    Change port

--- server.js
+++ server.js
@@ -66,6 +66,6 @@ app.post("/imageurl", auth.getAuthentication, (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(8080, () => {
+app.listen(80, () => {
   console.log("app is running on port 8080");
 });
