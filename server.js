3813399a0cdbfdc1c867401065cbb85a8a2ad63a
Alon-Fabio <alon.the.fabio@gmail.com>
2021-03-05 11:19:44 +0200

    SSl update

--- server.js
+++ server.js
@@ -1,69 +1,16 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt-nodejs");
-const cors = require("cors");
-const knex = require("knex");
-const morgan = require("morgan");
-const path = require("path");
+"use strict";
 
-const register = require("./controllers/register");
-const signin = require("./controllers/signin");
-const profile = require("./controllers/profile");
-const image = require("./controllers/image");
-const auth = require("./controllers/auth");
+var app = require("./app.js");
 
-const { POSTGRES_URI } = require("./secret");
+require("greenlock-express")
+  .init({
+    packageRoot: __dirname,
+    configDir: "./greenlock.d",
 
-const whitelist = [
-  "https://www.alonfabio.com",
-  "https://multitasker.alonfabio.com",
-  "http://multitasker.alonfabio.com",
-];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS " + origin));
-    }
-  },
-};
+    maintainerEmail: "alon.the.fabio@gmail.com",
 
-const db = knex({
-  // connect to your own database here.
-  client: "postgres",
-  connection: process.env.POSTGRES_URI || POSTGRES_URI,
-});
-
-const app = express();
-
-app.use(morgan("combined"));
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-app.post("/signin", signin.signinAuthentication(db, bcrypt));
-app.post("/register", (req, res) => {
-  register.handleRegister(req, res, db, bcrypt);
-});
-app.get("/profile/:id", auth.getAuthentication, (req, res) => {
-  profile.handleProfileGet(req, res, db);
-});
-app.post("/profile/:id", auth.getAuthentication, (req, res) => {
-  profile.handleProfilePost(req, res, db);
-});
-app.put("/image", auth.getAuthentication, (req, res) => {
-  image.handleImage(req, res, db);
-});
-app.post("/imageurl", auth.getAuthentication, (req, res) => {
-  image.handleApiCall(req, res);
-});
-
-app.listen(8080, () => {
-  console.log("app is running on port 8080");
-});
-
-module.exports = app;
+    // whether or not to run at cloudscale
+    cluster: false,
+  })
+  // Serves on 80 and 443
+  .serve(app);
